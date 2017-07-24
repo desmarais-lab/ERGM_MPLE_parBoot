e1fcfa92b4bf9e40bb60ea640c06f9006a6adfd3
Christian <cxs5700@psu.edu>
2017-07-24 19:47:42 -0400

    Relative time code and new plot
    newest version of the code for the relative time plot

--- Cosponsorship/Relative Time plot.R
+++ Cosponsorship/Relative Time plot.R
@@ -26,32 +26,37 @@ set.seed(1)
 for(i in 1:20){
   print(i)
   b<- system.time(ergm(faux.mesa.high~edges+nodefactor("Sex")+gwesp(0.25,fixed=TRUE), 
-                       eval.loglik = FALSE, control = control.ergm(MCMC.interval = 2000,MCMC.samplesize=1000)))
+                       eval.loglik = FALSE, control = control.ergm(MCMC.interval = 2000,MCMC.samplesize=2000)))
   time.mcmle.mesa[i]<- b[3]                  
 }
 
 median(time.mcmle.mesa)
 
-
-time.mple<- median(time.mple.mesa)
-time.mcmle<- median(time.mcmle.mesa)
-time.sim<- median(time.mcmle.mesa)/5 #takes 2 iterations, MCMC samplesize is quadrupled in last iteration
+time.mple.mesa.med<- median(time.mple.mesa)
+time.mcmle.mesa.med<- median(time.mcmle.mesa)
+time.sim.mesa.med<- median(time.mcmle.mesa)/5 #takes 2 iterations, MCMC samplesize is quadrupled in last iteration
 
 # calculate bootMPLE time for 500 iterations and different number of cores (1, 5, 10, 25, 50, 100, 250, 500)
 
 boot.mple.mesa<-c()
-boot.mple.mesa[1]<- 500*time.mple/1 + time.sim
-boot.mple.mesa[2]<- 500*time.mple/5 + time.sim
-boot.mple.mesa[3]<- 500*time.mple/10 + time.sim
-boot.mple.mesa[4]<- 500*time.mple/25 + time.sim
-boot.mple.mesa[5]<- 500*time.mple/50 + time.sim
-boot.mple.mesa[6]<- 500*time.mple/100 + time.sim
-boot.mple.mesa[7]<- 500*time.mple/250 + time.sim
-boot.mple.mesa[8]<- 500*time.mple/500 + time.sim
+boot.mple.mesa[1]<- 500*time.mple.mesa.med/1 + time.sim.mesa.med
+boot.mple.mesa[2]<- 500*time.mple.mesa.med/2 + time.sim.mesa.med
+boot.mple.mesa[3]<- 500*time.mple.mesa.med/3 + time.sim.mesa.med
+boot.mple.mesa[4]<- 500*time.mple.mesa.med/4 + time.sim.mesa.med
+boot.mple.mesa[5]<- 500*time.mple.mesa.med/5 + time.sim.mesa.med
+boot.mple.mesa[6]<- 500*time.mple.mesa.med/6 + time.sim.mesa.med
+boot.mple.mesa[7]<- 500*time.mple.mesa.med/7 + time.sim.mesa.med
+boot.mple.mesa[8]<- 500*time.mple.mesa.med/8 + time.sim.mesa.med
 
 #relative time
 
-rel.time.mesa<- boot.mple.mesa/time.mcmle
+rel.time.mesa<- boot.mple.mesa/time.mcmle.mesa.med
+
+# where does the ratio converge to?
+more.cores <- 1:100
+boot.mple.mesa.long<- 500*time.mple.mesa.med/more.cores + time.sim.mesa.med
+rel.mesa.long<- boot.mple.mesa.long/time.mcmle.mesa.med
+rel.mesa.long # 0.218
 
 ########################################
 # magnolia high
@@ -71,7 +76,7 @@ median(time.mple.magno)
 time.mcmle.magno<- c() # for storing the time
 
 set.seed(1)
-for(i in 1:4){
+for(i in 1:5){
   print(i)
   b<- system.time(ergm(faux.magnolia.high~edges+nodefactor("Sex")+gwesp(0.25,fixed=TRUE), 
                        eval.loglik = FALSE,control=control.ergm(MCMC.interval = 5000,MCMC.samplesize=8000)))
@@ -82,25 +87,31 @@ median(time.mcmle.magno)
 
 
 
-time.mple<- median(time.mple.magno)
-time.mcmle <- median(time.mcmle.magno)
-time.sim<- median(time.mcmle.magno)/5 #takes 2 iterations, MCMC samplesize is quadrupled in last iteration
+time.mple.magno.med<- median(time.mple.magno)
+time.mcmle.magno.med <- median(time.mcmle.magno)
+time.sim.magno.med<- median(time.mcmle.magno)/6 #takes 3 iterations, MCMC samplesize is quadrupled in last iteration
 
 # calculate bootMPLE time for 500 iterations and different number of cores (1, 5, 10, 25, 50, 100, 250, 500)
+
 boot.mple.magnolia<-c()
-boot.mple.magnolia[1]<- 500*time.mple/1+ time.sim
-boot.mple.magnolia[2]<- 500*time.mple/5+ time.sim
-boot.mple.magnolia[3]<- 500*time.mple/10+ time.sim
-boot.mple.magnolia[4]<- 500*time.mple/25+ time.sim
-boot.mple.magnolia[5]<- 500*time.mple/50+ time.sim
-boot.mple.magnolia[6]<- 500*time.mple/100+ time.sim
-boot.mple.magnolia[7]<- 500*time.mple/250+ time.sim
-boot.mple.magnolia[8]<- 500*time.mple/500+ time.sim
+boot.mple.magnolia[1]<- 500*time.mple.magno.med/1 + time.sim.magno.med
+boot.mple.magnolia[2]<- 500*time.mple.magno.med/2 + time.sim.magno.med
+boot.mple.magnolia[3]<- 500*time.mple.magno.med/3 + time.sim.magno.med
+boot.mple.magnolia[4]<- 500*time.mple.magno.med/4 + time.sim.magno.med
+boot.mple.magnolia[5]<- 500*time.mple.magno.med/5 + time.sim.magno.med
+boot.mple.magnolia[6]<- 500*time.mple.magno.med/6 + time.sim.magno.med
+boot.mple.magnolia[7]<- 500*time.mple.magno.med/7 + time.sim.magno.med
+boot.mple.magnolia[8]<- 500*time.mple.magno.med/8 + time.sim.magno.med
 
 #relative time
 
-rel.time.magnolia<- boot.mple.magnolia/time.mcmle
+rel.time.magnolia<- boot.mple.magnolia/time.mcmle.magno.med
 
+# where does the ratio converge to?
+more.cores <- 1:100
+boot.mple.magnolia.long<- 500*time.mple.magno.med/more.cores + time.sim.magno.med
+rel.magnolia.long<- boot.mple.magnolia.long/time.mcmle.magno.med
+rel.magnolia.long # 0.188
 
 
 
@@ -126,131 +137,68 @@ median(time.mple.co)
 time.mcmle.co<- c() # for storing the time
 
 set.seed(1)
-for(i in 1:10){
+for(i in 1:5){
   print(i)
   b<- system.time(ergm(cosponsorNetwork~edges+nodematch("sponsorParty")+altkstar(0.4975,fixed=T), 
-                       eval.loglik = FALSE, control = control.ergm(MCMC.interval = 30000, MCMC.burnin = 300000,MCMC.samplesize=20000)))
+                       eval.loglik = FALSE, control = control.ergm(MCMC.interval = 30000, MCMC.samplesize=10000)))
   time.mcmle.co[i]<- b[3]                  
 }
 
 median(time.mcmle.co)
 
-time.mple<- median(time.mple.co)
-time.mcmle<-median(time.mcmle.co)
-time.sim <- median(time.mcmle.co)/5 # takes 2 iterations, MCMC samplesize is quadrupled in last iteration
+time.mple.co.med<- median(time.mple.co)
+time.mcmle.co.med <- median(time.mcmle.co)
+time.sim.co.med<- median(time.mcmle.co)/5 #takes 2 iterations, MCMC samplesize is quadrupled in last iteration
 
-# calculate bootMPLE time for 500 iterations and different number of cores (1, 5, 10, 25, 50, 100, 250, 500)
 boot.mple.co<-c()
-boot.mple.co[1]<- 500*time.mple/1 + time.sim
-boot.mple.co[2]<- 500*time.mple/5+ time.sim
-boot.mple.co[3]<- 500*time.mple/10+ time.sim
-boot.mple.co[4]<- 500*time.mple/25+ time.sim
-boot.mple.co[5]<- 500*time.mple/50+ time.sim
-boot.mple.co[6]<- 500*time.mple/100+ time.sim
-boot.mple.co[7]<- 500*time.mple/250+ time.sim
-boot.mple.co[8]<- 500*time.mple/500+ time.sim
-
+boot.mple.co[1]<- 500*time.mple.co.med/1 + time.sim.co.med
+boot.mple.co[2]<- 500*time.mple.co.med/2 + time.sim.co.med
+boot.mple.co[3]<- 500*time.mple.co.med/3 + time.sim.co.med
+boot.mple.co[4]<- 500*time.mple.co.med/4 + time.sim.co.med
+boot.mple.co[5]<- 500*time.mple.co.med/5 + time.sim.co.med
+boot.mple.co[6]<- 500*time.mple.co.med/6 + time.sim.co.med
+boot.mple.co[7]<- 500*time.mple.co.med/7 + time.sim.co.med
+boot.mple.co[8]<- 500*time.mple.co.med/8 + time.sim.co.med
 
 #relative time
 
-rel.time.co<- boot.mple.co/time.mcmle
+rel.time.co<- boot.mple.co/time.mcmle.co.med
 
-
-# plot
-par(mfrow=c(1,1))
-
-plot(1:8, rel.time.mesa, main="Relative Time", xlab="Number of Cores", ylab="Relative Time", type="l", xaxt="n", 
-     col="blue", lwd=2, cex.main=1.5, cex.axis=1.5, cex.lab=1.5, ylim=c(0,2.3))
-axis(1, 1:8, label=c(1,5,10,25, 50,100,250,500))
-lines(1:8, rel.time.magnolia, col="red", lwd=2)
-lines(1:8, rel.time.co, col="darkgreen", lwd=2)
-abline(h=1,col="grey", lty=2, lwd=2)
-
-legend("topright", inset=c(.05, .01), legend=c("Faux Mesa High","Faux Magnolia High", "Cosponsorship"),
-       lty = 1,lwd=2, col = c("blue", "red", "darkgreen"), cex=1.5, bty="n")
-
+# where does the ratio converge to?
+more.cores <- 1:100
+boot.mple.co.long<- 500*time.mple.co.med/more.cores + time.sim.co.med
+rel.co.long<- boot.mple.co.long/time.mcmle.co.med
+rel.co.long #0.215
 
 
 
 ################################
-## relative time less cores
+## Plots
 #################################
 
-# comparison plot MCMLE vs MPLE
-
-# mesa high
-
-time.mple<- median(time.mple.mesa)
-time.mcmle<- median(time.mcmle.mesa)
-time.sim<- median(time.mcmle.mesa)/5 #takes 2 iterations, MCMC samplesize is quadrupled in last iteration
-
-# calculate bootMPLE time for 500 iterations and different number of cores (1, 5, 10, 25, 50, 100, 250, 500)
-
-boot.mple.mesa<-c()
-boot.mple.mesa[1]<- 500*time.mple/1 + time.sim
-boot.mple.mesa[2]<- 500*time.mple/2 + time.sim
-boot.mple.mesa[3]<- 500*time.mple/3 + time.sim
-boot.mple.mesa[4]<- 500*time.mple/4 + time.sim
-boot.mple.mesa[5]<- 500*time.mple/5 + time.sim
-boot.mple.mesa[6]<- 500*time.mple/6 + time.sim
-boot.mple.mesa[7]<- 500*time.mple/7 + time.sim
-boot.mple.mesa[8]<- 500*time.mple/8 + time.sim
-
-#relative time
-
-rel.time.mesa<- boot.mple.mesa/time.mcmle
-
-# magnolia high
-
-time.mple<- median(time.mple.magno)
-time.mcmle <- median(time.mcmle.magno)
-time.sim<- median(time.mcmle.magno)/5 #takes 2 iterations, MCMC samplesize is quadrupled in last iteration
-
-# calculate bootMPLE time for 500 iterations and different number of cores (1, 5, 10, 25, 50, 100, 250, 500)
-boot.mple.magnolia<-c()
-boot.mple.magnolia[1]<- 500*time.mple/1+ time.sim
-boot.mple.magnolia[2]<- 500*time.mple/2+ time.sim
-boot.mple.magnolia[3]<- 500*time.mple/3+ time.sim
-boot.mple.magnolia[4]<- 500*time.mple/4+ time.sim
-boot.mple.magnolia[5]<- 500*time.mple/5+ time.sim
-boot.mple.magnolia[6]<- 500*time.mple/6+ time.sim
-boot.mple.magnolia[7]<- 500*time.mple/7+ time.sim
-boot.mple.magnolia[8]<- 500*time.mple/8+ time.sim
-
-#relative time
-
-rel.time.magnolia<- boot.mple.magnolia/time.mcmle
 
 
-# relative time cosponsorship
+# plot
+plot(1:8, rel.mesa.long[1:8], main="Relative Time", xlab="Number of Cores", ylab="Relative Time", type="l", xaxt="n", 
+     col="blue", lwd=2, cex.main=1.5, cex.axis=1.5, cex.lab=1.5, ylim=c(0.2,2.3))
+axis(1, 1:8, label=1:8, cex.axis=1.5)
+lines(1:8, rel.magnolia.long[1:8], col="red", lwd=2)
+lines(1:8, rel.co.long[1:8], col="darkgreen", lwd=2)
+abline(h=1,col="grey", lty=2, lwd=2)
 
-time.mple<- median(time.mple.co)
-time.mcmle<-median(time.mcmle.co)
-time.sim <- median(time.mcmle.co)/5 # takes 2 iterations, MCMC samplesize is quadrupled in last iteration
+legend("topright", inset=c(.05, .01), legend=c("Faux Mesa High","Faux Magnolia High", "Cosponsorship"),lty = 1,lwd=2, col = c("blue", "red", "darkgreen"), cex=1.5, bty="n")
+# 7x10 inches
 
-# calculate bootMPLE time for 500 iterations and different number of cores (1, 5, 10, 25, 50, 100, 250, 500)
-boot.mple.co<-c()
-boot.mple.co[1]<- 500*time.mple/1 + time.sim
-boot.mple.co[2]<- 500*time.mple/2+ time.sim
-boot.mple.co[3]<- 500*time.mple/3+ time.sim
-boot.mple.co[4]<- 500*time.mple/4+ time.sim
-boot.mple.co[5]<- 500*time.mple/5+ time.sim
-boot.mple.co[6]<- 500*time.mple/6+ time.sim
-boot.mple.co[7]<- 500*time.mple/7+ time.sim
-boot.mple.co[8]<- 500*time.mple/8+ time.sim
 
-#relative time
-
-rel.time.sc<- boot.mple.co/time.mcmle
-
+##################################
+# plot for presentation
 
 # plot
 plot(1:8, rel.time.mesa, main="Relative Time", xlab="Number of Cores", ylab="Relative Time", type="l", xaxt="n", 
      col="blue", lwd=2, cex.main=1.5, cex.axis=1.5, cex.lab=1.5, ylim=c(0.2,2.3))
 axis(1, 1:8, label=1:8, cex.axis=1.5)
 lines(1:8, rel.time.magnolia, col="red", lwd=2)
-lines(1:8, rel.time.co, col="darkgreen", lwd=2)
 abline(h=1,col="grey", lty=2, lwd=2)
 
-legend("topright", inset=c(.05, .01), legend=c("Faux Mesa High","Faux Magnolia High", "Cosponsorship"),lty = 1,lwd=2, col = c("blue", "red", "darkgreen"), cex=1.5, bty="n")
+legend("topright", inset=c(.05, .01), legend=c("Mesa High","Magnolia High"),lty = 1,lwd=2, col = c("blue", "red"), cex=1.5, bty="n")
 # 7x10 inches
