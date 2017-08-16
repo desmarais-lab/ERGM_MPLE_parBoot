471d93031e0e440fe7013f2742994194bd789d85
Christian <cxs5700@psu.edu>
2017-08-16 14:54:42 -0400

    revised code
    added a new column to degeneracy.check: degenerate? True/False
    library(ergm) does not have to be loaded for every foreach iterations
    changed the example to the example used in the paper: faux.mesa.high~ edges+nodematch("Sex")+ gwesp(0.25, fixed=TRUE)

--- boot_MPLE.R
+++ boot_MPLE.R
@@ -13,11 +13,11 @@
 # mcmc.burnin: default=16*1024, Number of proposals before any MCMC sampling is done. It typically is set to a fairly large number
 # number.cores: number of cores used in caluclation
 boot.MPLE<- function(ergm.formula, boot.rep=500, mcmc.interval=1024, mcmc.burnin=16*1024, number.cores=1){
-	
-	require(statnet)
-	require(doParallel)
-	require(foreach)
-	require(ergm)
+  
+  require(statnet)
+  require(doParallel)
+  require(foreach)
+  require(ergm)
   
   # check whether multiple cores are necessary
   if(number.cores>1){
@@ -29,9 +29,6 @@ boot.MPLE<- function(ergm.formula, boot.rep=500, mcmc.interval=1024, mcmc.burnin
   # save network statistics of observed network
   observed.network.stats <- mple$target.stats
   
-  # extract network from formula
-  net <- terms(ergm.formula)[[2]]
- 
   # get mple coefficients
   mple.coef <- summary(mple)[[20]]
   # get number of parameters
@@ -50,31 +47,32 @@ boot.MPLE<- function(ergm.formula, boot.rep=500, mcmc.interval=1024, mcmc.burnin
   
   
   # loop (can be parallelized) to calculate the the mple of the simulated networks 
-  boot.coef<- foreach(i= 1:boot.rep, .combine=cbind)%dopar%{
-  
-  ### foreach requires to load ergm package again  
-  library(ergm)
-  # extract covariates
-  covariates <- labels(terms(ergm.formula))
-  # create auxiliary formula
-  aux.formula <- sim.mple[[i]]~a+b
-  # create formula for simulated networks
-  sim.formula<-reformulate(covariates, aux.formula[[2]])
-  
-  # calculate MPLE of simulated network and get coefficients
-  sim.results <- ergm(sim.formula, estimate= "MPLE")
-  coeffs<-coef(sim.results)
-  # calculate statistics of simulated networks
-  sim.network.stats <- sim.results$target.stats
-  
-  # return
-  c(coeffs, sim.network.stats)
+  boot.coef<- foreach(i= 1:boot.rep, .combine=cbind, .packages=c("ergm"))%dopar%{
+
+    # extract covariates
+    covariates <- labels(terms(ergm.formula))
+    # create auxiliary formula
+    aux.formula <- sim.mple[[i]]~a+b
+    # create formula for simulated networks
+    sim.formula<-reformulate(covariates, aux.formula[[2]])
+    
+    # calculate MPLE of simulated network and get coefficients
+    sim.results <- ergm(sim.formula, estimate= "MPLE")
+    coeffs<-coef(sim.results)
+    # calculate statistics of simulated networks
+    sim.network.stats <- sim.results$target.stats
+    
+    # return
+    c(coeffs, sim.network.stats)
   }
   
   # create empty matrix to store results of the degeneracy test
   degeneracy.test<- cbind(mple.coef, c(1:number.parameters))
-  colnames(degeneracy.test)<- c("sim > obs", "obs", "sim 25%", "sim 50%", "sim 75%")
+  colnames(degeneracy.test)<- c("sim > obs", "observed_value", "sim 25%", "sim 50%", "sim 75%")
+  degeneracy.test <- as.data.frame(degeneracy.test)
   
+  # empty vector for true/false
+  ev<- c()
   # calculate 2.5 percentile and 97.5 percentile and degeneracy results
   for(j in 1:number.parameters){
     
@@ -83,12 +81,16 @@ boot.MPLE<- function(ergm.formula, boot.rep=500, mcmc.interval=1024, mcmc.burnin
     
     # proportion the simulated statistic is greater than the observed statistic. if result is close to 0 or 1 -> strong indicator for degeneracy
     # or increase mcmc.interval and mcmc.burnin
-    degeneracy.test[j,1]<- length(boot.coef[j+number.parameters, ][boot.coef[j+number.parameters, ]> observed.network.stats])/boot.rep
+    degeneracy.test[j,1]<- length(boot.coef[j+number.parameters, ][boot.coef[j+number.parameters, ]> observed.network.stats[j]])/boot.rep
     degeneracy.test[j,2]<- observed.network.stats[j]
     degeneracy.test[j,3:5] <- quantile(boot.coef[j+number.parameters, ], probs = c(0.25, 0.5, 0.75))
-    
-    }
-
+    # if sim > obs is 0 or 1, report TRUE, otherwise false. this is a strong indicator for degeneracy
+    ev[j] <- testing$degeneracy_check[j,1]==1 | testing$degeneracy_check[j,1]==0
+  }
+  
+  # put degeneracy test-matrix and ev-data vector together
+  degeneracy.test <- data.frame(degeneracy.test, ev)
+  colnames(degeneracy.test)<- c("sim > obs", "observed_value", "sim 25%", "sim 50%", "sim 75%", "degenerate?")
   # create list to return results
   results <- list()
   results[[1]]<- summary(mple)  # MPLE results
@@ -106,12 +108,12 @@ library(ergm)
 data(faux.mesa.high)
 # set seed to assure replicability
 set.seed(1234)
-testing<- boot.MPLE(ergm.formula=faux.mesa.high~edges+nodematch("Sex"), boot.rep=100, number.cores=2 )\
+testing<- boot.MPLE(ergm.formula=faux.mesa.high~edges+nodematch("Sex")+gwesp(0.25, fixed=TRUE), boot.rep=100, number.cores=2 )
 # MPLE results
 testing$MPLE_results
 
+# bootstrapped MPLE results
+testing$bootstrapped_MPLE_results
+
 # Degeneracy check
 testing$degeneracy_check
-
-
-
